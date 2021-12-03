f7a13e89ffef473a5735982bfcce7d36ee801b4c
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2021-12-03 14:31:51 +0200

    My commit message

--- expressServer.js
+++ expressServer.js
@@ -21,8 +21,6 @@ class ExpressServer {
       this.app.use(express.static(__dirname));
       this.app.get('/Page1', (req, res) => res.sendFile(path.join(__dirname,'html','Page1.html')));
 			this.app.get('/Page1/:id', (req, res) => res.sendFile(path.join(__dirname,'html','Page1.html')));
-			this.app.get('/Page2', (req, res) => res.sendFile(path.join(__dirname,'html','Page2.html')));
-			this.app.get('/Page2/:id', (req, res) => res.sendFile(path.join(__dirname,'html','Page2.html')));
 			
     }
   
